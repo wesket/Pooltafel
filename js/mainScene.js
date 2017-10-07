f037570dfbe7a6d26e8c70c17e591e6f7890c76c
Pieter <pietergwestra@hotmail.com>
2017-10-07 18:21:56 +0200

    Revert "Further improved ball physics behaviour"
    Uit mijn branch blijven in het vervolg aub...
    
    This reverts commit 440d5e50ab024685d4395c572fa0b8e3f4d88e28.

--- js/mainScene.js
+++ js/mainScene.js
@@ -15,50 +15,16 @@ class MainScene {
     var keuGeometry = new THREE.CylinderGeometry(0.025, 0.05, 4, 32, 32),
         keuMaterial = new THREE.MeshStandardMaterial({ color: 0xfda43a }),
         keuMesh = new THREE.Mesh(keuGeometry, keuMaterial);
-    keuMesh.position.y= 0.3;
-    keuMesh.rotateX(Math.PI/2);
-    keuMesh.position.z -=3.6;
-    keuMesh.rotateX(0.1);
-
-    this.cue = new THREE.Group();
-    this.cue.add(keuMesh);
-    this.cue.position.set(0, 0.10, -6,75);
-
-    let randomStartRotation = 0.07;
-    this.cue.rotateY(randomStartRotation/2 -Math.random() * randomStartRotation);
-
-
-
-    scene.add(this.cue);
+    keuGeometry.translate(0, -2, 2);
 
 
     scene.add(keuMesh);
 
     balls = [
-        new Ball(0, -1.5, 0.10,0, false),
-
-        new Ball(0, 1.3, 0.10, 1, false),
-
-        new Ball(-0.10, 1.5, 0.10, 2, false),
-        new Ball(0.10, 1.5, 0.10, 3, false),
-
-        new Ball(0, 1.7, 0.10, 8, false),
-        new Ball(0.20, 1.7, 0.10, 5, false),
-        new Ball(-0.20, 1.7, 0.10, 6, false),
-
-        new Ball(0.30, 1.9, 0.10, 7, false),
-        new Ball(-0.30, 1.9, 0.10, 4, false),
-        new Ball(0.10, 1.9,0.10,  9, true),
-        new Ball(-0.10, 1.9, 0.10, 10, true),
-
-        new Ball(0, 2.1, 0.10, 11, true),
-        new Ball(0.40, 2.1, 0.10, 12, true),
-        new Ball(-0.40, 2.1, 0.10, 13, true),
-        new Ball(0.20, 2.1, 0.10, 14, true),
-        new Ball(-0.20, 2.1, 0.10, 15, true)
+        new Ball(0, -1.5, 0.10, 0, false),
+        new Ball(0.1, 1.3, 0.10, 1, false),
+        new Ball(-0.1, 1.3, 0.10, 2, false)
     ];
-
-    wball = new Ball(0, -1.5, 0.10);
 
   }
 
